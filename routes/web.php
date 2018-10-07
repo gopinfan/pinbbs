913bb97893a327e216c9bd517c9e84346cc94a22
gopinfan <gopinfan@gmail.com>
2018-10-07 02:31:10 +0000

    生成器生成topics

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Route::get('/', 'IndexController@home')->name('home');
 Auth::routes();
 
 Route::resource('users', 'UsersController')->only(['show','update','edit']);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
