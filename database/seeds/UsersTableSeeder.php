7c024d2cb019f7fa411a8907efd3b7ac88ba528f
gopinfan <gopinfan@gmail.com>
2018-10-10 09:06:47 +0000

    填充用户和话题数据

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user,$index) use ($avatars){
+                $user->avatar = array_random($avatars);
+            });
+
+        $data = $users->makeVisible(['password','remember_token'])->toArray();
+
+        User::insert($data);
+
+        $user = User::find(1);
+        $user->name = 'esinger';
+        $user->email = 'esinger@qq.com';
+        $user->avatar = get_gravatar('esinger@qq.com');
+        $user->save();
+    }
+}
