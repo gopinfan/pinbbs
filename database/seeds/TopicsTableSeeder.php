19a285131502700d61d88ca9c455e5c3cc0e3fcc
gopinfan <gopinfan@gmail.com>
2018-10-10 09:05:24 +0000

    填充用户和话题数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -7,10 +7,12 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        $userIds = \App\Models\User::all()->pluck('id')->toArray();
+        $categoryIds = \App\Models\Category::all()->pluck('id')->toArray();
+
+        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) use ($userIds, $categoryIds) {
+            $topic->user_id = array_random($userIds);
+            $topic->category_id = array_random($categoryIds);
         });
 
         Topic::insert($topics->toArray());
