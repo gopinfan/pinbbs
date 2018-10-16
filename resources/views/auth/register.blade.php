ae1358b8bd2bef8aecf308142854b982dabfb0ac
gopinfan <gopinfan@gmail.com>
2018-10-16 05:12:49 +0000

    优化登录和注册页面的按钮

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -79,7 +79,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">注册</button>
+                                <button type="submit" class="btn btn-primary px-4">注册</button>
                             </div>
                         </div>
                     </form>
